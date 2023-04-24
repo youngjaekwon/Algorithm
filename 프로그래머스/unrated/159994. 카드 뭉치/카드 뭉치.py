cb46b1b9936753c0bc1ea206ff8e3407bbf52b96
youngjae kwon <60334115+youngjaekwon@users.noreply.github.com>
2023-04-24 22:17:34 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,14 @@
+from collections import deque
+
+
+def solution(cards1, cards2, goal):
+    cards1 = deque(cards1)
+    cards2 = deque(cards2)
+    for word in goal:
+        if cards1 and cards1[0] == word:
+            cards1.popleft()
+        elif cards2 and cards2[0] == word:
+            cards2.popleft()
+        else:
+            return "No"
+    return "Yes"
