8add47d9457fac30ae1cbac06b86ff6edb6fd71a
youngjae kwon <60334115+youngjaekwon@users.noreply.github.com>
2023-04-30 14:10:39 +0900

    [Bronze III] Title: 약수 구하기, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,11 @@
+N, M = list(map(int, input().split()))
+cnt = 0
+
+for i in range(1, N + 1):
+    if N % i == 0:
+        cnt += 1
+        if cnt == M:
+            print(i)
+            break
+else:
+    print(0)
