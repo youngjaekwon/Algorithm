562a36955e17cabf23f44260fda7f569b0d7ac35
youngjae kwon <60334115+youngjaekwon@users.noreply.github.com>
2024-07-07 20:58:58 +0900

    [Silver I] Title: 오목, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2615. 오목/오목.py
@@ -0,0 +1,54 @@
+arr = [list(map(int, input().split())) for _ in range(19)]
+
+"""
+[아래, 우측, 우측아래, 좌측아래]
+"""
+x_arr = [1, 0, 1, 1]
+y_arr = [0, 1, 1, -1]
+searched = set()
+
+
+def search(c: int, x: int, y: int) -> tuple[bool, tuple[int, int] | None]:
+    for d in range(4):
+        if (x, y, d) in searched:
+            continue
+        searched.add((x, y, d))
+        xp, yp = x + x_arr[d], y + y_arr[d]
+        if xp == 19 or yp == 19 or arr[xp][yp] != c or (xp, yp, d) in searched:
+            continue
+        searched.add((xp, yp, d))
+
+        cnt = 2
+        fxp, fyp = 0, 0
+        for i in range(4):
+            xp, yp = xp + x_arr[d], yp + y_arr[d]
+            if xp == 19 or yp == 19 or arr[xp][yp] != c or (xp, yp, d) in searched:
+                break
+            cnt += 1
+            fxp, fyp = xp, yp
+            searched.add((xp, yp, d))
+
+        if cnt == 5:
+            return True, (x, y) if d != 3 else (fxp, fyp)
+
+    return False, None
+
+
+result = []
+p = []
+
+for i in range(19):
+    for j in range(19):
+        v = arr[i][j]
+        if not v:
+            continue
+        r, rp = search(v, i, j)
+        if r:
+            result.append(v)
+            p.append((rp[0] + 1, rp[1] + 1))
+
+if len(result) == 1:
+    print(result[0])
+    print(p[0][0], p[0][1])
+else:
+    print(0)
