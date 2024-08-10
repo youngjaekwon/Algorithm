95111b5a978995d307130d1e790bbfb7f4d5d48c
youngjae kwon <60334115+youngjaekwon@users.noreply.github.com>
2024-08-10 14:31:28 +0900

    [Silver III] Title: 1로 만들기, Time: 556 ms, Memory: 39368 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,22 +1,13 @@
-from collections import deque
-
 N = int(input())
+memoization = [-1]
 
-queue = deque([(0, N)])
-answer = 0
+for i in range(1, N + 1):
+    counts = []
+    if i % 3 == 0:
+        counts.append(memoization[i // 3])
+    if i % 2 == 0:
+        counts.append(memoization[i // 2])
+    counts.append(memoization[i - 1])
+    memoization.append(min(counts) + 1)
 
-while True:
-    c, x = queue.popleft()
-    if x == 1:
-        answer = c
-        break
-
-    c += 1
-
-    if x % 3 == 0:
-        queue.append((c, x // 3))
-    if x % 2 == 0:
-        queue.append((c, x // 2))
-    queue.append((c, x - 1))
-
-print(answer)
+print(memoization[-1])
