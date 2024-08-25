b294a5d3f67bb29813493919ee82daba7ccde80e
youngjae kwon <gczzga@gmail.com>
2024-08-25 13:59:48 +0900

    [Gold V] Title: 강의실, Time: 2780 ms, Memory: 52308 KB -BaekjoonHub

---
+++ 백준/Gold/1374. 강의실/강의실.py
@@ -0,0 +1,18 @@
+N = int(input())
+arr = []
+
+for _ in range(N):
+    n, start, end = list(map(int, input().split()))
+    arr.append((start, 1))
+    arr.append((end, -1))
+
+arr.sort()
+
+answer = 0
+current = 0
+
+for _, v in arr:
+    current += v
+    answer = max(answer, current)
+
+print(answer)
